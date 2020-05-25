9c1ce6b60536989e6f9bc2311bbdceeb716a2cee
Horairy <horairyhakim@gmail.com>
2020-05-26 01:57:17 +0200

    Ajout de la feature 2 joueurs

--- tictactoe.py
+++ tictactoe.py
@@ -7,6 +7,7 @@ Author : Horairy Hakim
 ###fonctions utiles
 import os
 import random
+
 def rechercher(mot , lettre):
 	"""Cet recherche un caractère dans un string et renvoie une liste contenant les positions du caractère dans le string"""
 	compteur = 0
@@ -20,11 +21,16 @@ def rechercher(mot , lettre):
 
 
 def intialisationPartie() :
-	#Chargement de l'interface graphique
+	"""Cette fonction permet d'initialiser la partie en chargant l'interface graphique dans un fichier temporaire : "PartieEnCours".
+		Elle affiche la grille de début de partie sur le terminal.
+		Elle renvoit une liste de booléen décrivant l'état du damier :
+				True : Case occupée
+				False : Case vide
+		La partie étant initialisée la liste de contient donc que des False"""
 	interfaceGraphique = open("fichier", "r", encoding='UTF-8')
 	contenuInterfaceGraphique = interfaceGraphique.read()
 	interfaceGraphique.close()
-	#Création d'une interface pour la partie (à partir de l'interface chargée)
+	print(contenuInterfaceGraphique)
 	maPartie = open("partieEnCours", "w", encoding='UTF-8')
 	maPartie.write(contenuInterfaceGraphique)
 	maPartie.close()
@@ -32,11 +38,21 @@ def intialisationPartie() :
 
 	return etatPartie
 
+def caseVide(etatPartie):
+	""" Cette fonction prendre en entrée l'été des case du damier
+		et renvoie une liste contenant le numéro des cases vides du damier."""
+	caseVide = []
+	for k in range(9):
+		if etatPartie[k] == False:
+			caseVide.append(k)
+	return caseVide
+
 def ecrireGrille(nombreCase, symbole):
+	"""Cette fonction prends
+	"""
 	interfaceGraphique = open("partieEnCours", "r")
 	contenuInterfaceGraphique = interfaceGraphique.read()
 	interfaceGraphique.close()
-	print(contenuInterfaceGraphique)
 	case = rechercher(contenuInterfaceGraphique, (str(nombreCase)))
 	tailleLigne = rechercher(contenuInterfaceGraphique, ("\n"))[0]
 	inte = list(contenuInterfaceGraphique)
@@ -50,30 +66,21 @@ def ecrireGrille(nombreCase, symbole):
 	maPartie.close()
 	return None
 
-etatPartie = intialisationPartie()
-for k in range(9):
-	if k == 0 : 
-		contenuInterfaceGraphique = intialisationPartie()
-		ecrireGrille(0, "O")
-		etatPartie[0] = True
-	else :
-		if k % 2 == 1 :
-			nombreCase = input("entrez la case que vous voulez remplir: ")
-			ecrireGrille(nombreCase, "X")
-			etatPartie[int(nombreCase)] = True
-		else : 
-			caseVide = []
-			for k in range(9):
-				if etatPartie[k] == False:
-					caseVide.append(k)
-
-				#nombreCase = random.choice(caseVide)
-				#print(nombreCase)
-				#ecrireGrille(str(nombreCase), "O")
-				#etatPartie[int(nombreCase)] = True
-
-
-
-
 
 """Boucle de jeu"""
+os.system('cls' if os.name == 'nt' else 'clear')
+etatPartie = intialisationPartie()
+for k in range(9):
+	if k % 2 == 0 :
+		nombreCase = input("entrez la case que vous voulez remplir (Joueur 1) : ")
+		ecrireGrille(nombreCase, "X")
+		etatPartie[int(nombreCase)] = True
+	else : 
+		nombreCase = input("entrez la case que vous voulez remplir (Joueur 2) : ")
+		ecrireGrille(nombreCase, "O")
+		etatPartie[int(nombreCase)] = True
+
+
+
+
+
